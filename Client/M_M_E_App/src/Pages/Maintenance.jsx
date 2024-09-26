f4a60e7c8f69a5a6568895392beef7d040bd68b3
Thiemoko <148875110+Thiemoko27@users.noreply.github.com>
2024-09-26 13:27:45 +0000

    Update Maintenance.jsx

--- Client/M_M_E_App/src/Pages/Maintenance.jsx
+++ Client/M_M_E_App/src/Pages/Maintenance.jsx
@@ -1,6 +1,6 @@
 export function Maintenance () {
     return(
-        <div className="container">
+        <div className="container" style={{textAlign: 'center'}}>
             <h1>Le site est actuellement en maintenance</h1>
         </div>
     )
